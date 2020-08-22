f9efd785b2f60d15447a5ca2d098fc332ebd177f
selinahsu <selinaraindrop@gmail.com>
2020-08-22 14:18:21 -0400

    no react yet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
@@ -16,8 +17,16 @@ app.get('/', (req, res) =>
   })
 );
 
-// Members API Routes
-//app.use('/api/members', require('./routes/api/members'));
+app.post('/api/distance', (req, res) => {
+  const destination = {id: req.body.params.destination};
+  if (!destination) res.status(404).send('Oops! Something went wrong.');
+  console.log("i am here");
+  res.send({
+    distance: res.locals.distance,
+    emissions: res.locals.emissions,
+    fuel: res.locals.fuel
+  });
+});
 
 
 const port = process.env.PORT || 5000; 
